6e14f92e799f720b1e29f87dafc3f8c86a2fac76
hyeonaaaa <qorwh5218@naver.com>
2022-07-12 16:27:54 +0900

    css

--- zetaplan/src/page/Company/CompanyCollabo.jsx
+++ zetaplan/src/page/Company/CompanyCollabo.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
+import '../../css/Company/companycollabo.css'
+
 
 const CompanyCollabo = () => {
   return (
     <div>
       <h1>협력기관</h1>
       <ul className='company_list'>
-        <li><img src="../../../public/img/mainFullpage/mainFullpageFooter/FooterCompanyList/list01.png" alt="" /><a href="/"></a></li>
-        <li><img src="../../../public/img/mainFullpage/mainFullpageFooter/FooterCompanyList/list02.png" alt="" /><a href="/"></a></li>
-        <li><img src="../../../public/img/mainFullpage/mainFullpageFooter/FooterCompanyList/list03.png" alt="" /><a href="/"></a></li>
-        <li><img src="../../../public/img/mainFullpage/mainFullpageFooter/FooterCompanyList/list04.png" alt="" /><a href="/"></a></li>
-        <li><img src="../../../public/img/mainFullpage/mainFullpageFooter/FooterCompanyList/list05.png" alt="" /><a href="/"></a></li>
+        <li></li>
       </ul>
     </div>
   );
