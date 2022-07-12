150c5e2d7b4a5eae8a440c6677299919548255f3
hyeonaaaa <qorwh5218@naver.com>
2022-07-12 16:05:32 +0900

    수정

--- zetaplan/src/page/Company/CompanyCollabo.jsx
+++ zetaplan/src/page/Company/CompanyCollabo.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const CompanyCollabo = () => {
   return (
     <div>
-      
+      <h1>협력기관</h1>
+      <ul className='company_list'>
+        <li><img src="../../../public/img/mainFullpage/mainFullpageFooter/FooterCompanyList/list01.png" alt="" /><a href="/"></a></li>
+        <li><img src="../../../public/img/mainFullpage/mainFullpageFooter/FooterCompanyList/list02.png" alt="" /><a href="/"></a></li>
+        <li><img src="../../../public/img/mainFullpage/mainFullpageFooter/FooterCompanyList/list03.png" alt="" /><a href="/"></a></li>
+        <li><img src="../../../public/img/mainFullpage/mainFullpageFooter/FooterCompanyList/list04.png" alt="" /><a href="/"></a></li>
+        <li><img src="../../../public/img/mainFullpage/mainFullpageFooter/FooterCompanyList/list05.png" alt="" /><a href="/"></a></li>
+      </ul>
     </div>
   );
 };
