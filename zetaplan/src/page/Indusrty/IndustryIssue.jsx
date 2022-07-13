ce02b059cab98085eb6d7c936ba82968139f79a6
INJI <hoye7737@gmail.com>
2022-07-13 17:28:17 +0900

    info 연결하기/ 이슈 게시판 작업 시작

--- zetaplan/src/page/Indusrty/IndustryIssue.jsx
+++ zetaplan/src/page/Indusrty/IndustryIssue.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import DetailFooter from '../../component/Detail/DetailFooter';
 import IndustryHeader from '../../component/Detail/IndustryHeader';
+import IssueList from './IndustryIssue/IssueList';
+
 
 const IndustryIssue = ({title, sub}) => {
   return (
-    <div>
+    <div id = 'IndustryIssue'>
       <IndustryHeader title={title} sub={sub} />
+      <div className="sub-inner">
+        <IssueList />
+      </div>
       <DetailFooter />
     </div>
   );
