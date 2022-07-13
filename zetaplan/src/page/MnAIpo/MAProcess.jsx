323a174ada14b857c5039d5cb002dd2d462cad87
phzzle <phzzle@gmail.com>
2022-07-13 17:32:44 +0900

    modify css

--- zetaplan/src/page/MnAIpo/MAProcess.jsx
+++ zetaplan/src/page/MnAIpo/MAProcess.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import MAHeader from '../../component/Detail/MAHeader';
-import DetailFooter from './../../component/Detail/DetailFooter';
+import React from "react";
+import MAHeader from "../../component/Detail/MAHeader";
+import DetailFooter from "./../../component/Detail/DetailFooter";
+import "./../../css/MnAIpo/mnaprocess.css";
 
 const MAProcess = ({title, sub}) => {
   return (
-    <div>
-      <MAHeader title={title} sub={sub}/>
+    <div id="mnaprocess">
+      <MAHeader title={title} sub={sub} />
+      <div id="SubInner">
+        <section className="mnaipo-maprocess-section1">
+          <div className="mnaipo-maprocess-title-container">
+            <h2 className="mnaipo-maprocess-title">M&A 컨설팅 과정</h2>
+            <div className="mnaipo-maprocess-title-line"></div>
+          </div>
+        </section>
+      </div>
       <DetailFooter />
     </div>
   );
