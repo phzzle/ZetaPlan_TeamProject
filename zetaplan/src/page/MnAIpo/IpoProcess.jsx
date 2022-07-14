aa08f85d9e0c0d54613a7db18e0d1d79f0ded009
phzzle <phzzle@gmail.com>
2022-07-14 15:16:50 +0900

    modify css,jsx

--- zetaplan/src/page/MnAIpo/IpoProcess.jsx
+++ zetaplan/src/page/MnAIpo/IpoProcess.jsx
@@ -1,11 +1,79 @@
-import React from 'react';
-import MAHeader from '../../component/Detail/MAHeader';
-import DetailFooter from './../../component/Detail/DetailFooter';
+import React from "react";
+import MAHeader from "../../component/Detail/MAHeader";
+import DetailFooter from "./../../component/Detail/DetailFooter";
+import "./../../css/MnAIpo/ipoprocess.css";
 
 const IpoProcess = ({title, sub}) => {
   return (
-    <div>
-      <MAHeader title={title} sub={sub}/>
+    <div id="ipoprocess">
+      <MAHeader title={title} sub={sub} />
+      <div id="SubInner">
+        <section className="mnaipo-ipoprocess-section1">
+          <div className="mnaipo-ipoprocess-title-container">
+            <h2 className="mnaipo-ipoprocess-title">IPO 컨설팅 과정</h2>
+            <div className="mnaipo-ipoprocess-title-line"></div>
+          </div>
+        </section>
+        <section className="mnaipo-ipoprocess-section">
+          <div className="mnaipo-ipoprocess-section-img"></div>
+        </section>
+        <div className="flex">
+          <section className="mnaipo-ipoprocess-section">
+            <div>
+              <h1 className="mnaipo-ipoprocess-part">STEP 1. 사전준비</h1>
+              <ul>
+                <li>기업공개 및 상장시기결정 </li>
+                <li>상장준비추진팀 구성</li>
+                <li>최대주주 등의 지분변동 사전점검</li>
+                <li>회계감사인의 선정 및 감리 </li>
+                <li>대표 주관회사의 선정</li>
+                <li>정관정비</li>
+                <li>명의개서대행계약체결</li>
+                <li>우리사주조합결정</li>
+                <li>상장을 위한 이사회 또는 주총 결의</li>
+                <li>최대주주 등의 소유주식등 보호 예수</li>
+              </ul>
+            </div>
+          </section>
+          <section className="mnaipo-ipoprocess-section">
+            <div>
+              <h1 className="mnaipo-ipoprocess-part">STEP 2. 상장예비심사</h1>
+              <ul>
+                <li>상장예비심사 청구</li>
+                <li>상장예비심사 규모요건</li>
+                <li>상장예비심사 분산 요건</li>
+                <li>상장예비심사 재무 요건</li>
+                <li>상장예비심사 안정성 및 건전성 요건</li>
+                <li>상장예비심사 실질적 심사 기준</li>
+                <li>상장위원회 심의</li>
+                <li>상장예비심사 결과 통지</li>
+              </ul>
+            </div>
+          </section>
+          <section className="mnaipo-ipoprocess-section">
+            <div>
+              <h1 className="mnaipo-ipoprocess-part">STEP 3. 공모</h1>
+              <ul>
+                <li>유가증권신고서 제출</li>
+                <li>예비 투자 설명서 제출</li>
+                <li>정정신고서 제출 및 유가증권신고서 효력발생</li>
+                <li>투자 설명서 비치, 교부</li>
+                <li>수요예측 및 공모가격 결정</li>
+                <li>청약,배정 및 납입</li>
+              </ul>
+            </div>
+          </section>
+          <section className="mnaipo-ipoprocess-section">
+            <div>
+              <h1 className="mnaipo-ipoprocess-part">STEP 4. 상장 및 매매</h1>
+              <ul>
+                <li>상장신청서 제출 및 상장승인 통보</li>
+                <li>기준가격 결정 및 매매거래 개시</li>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </div>
       <DetailFooter />
     </div>
   );
--- zetaplan/src/page/MnAIpo/IpoSummary.jsx
+++ zetaplan/src/page/MnAIpo/IpoSummary.jsx
@@ -1,11 +1,118 @@
-import React from 'react';
-import MAHeader from '../../component/Detail/MAHeader';
-import DetailFooter from './../../component/Detail/DetailFooter';
+import React from "react";
+import MAHeader from "../../component/Detail/MAHeader";
+import DetailFooter from "./../../component/Detail/DetailFooter";
+import "./../../css/MnAIpo/iposummary.css";
 
 const IpoSummary = ({title, sub}) => {
   return (
-    <div>
-      <MAHeader title={title} sub={sub}/>
+    <div id="iposummary">
+      <MAHeader title={title} sub={sub} />
+      <div id="SubInner">
+        <section className="mnaipo-iposummary-section1">
+          <div className="mnaipo-iposummary-title-container">
+            <h2 className="mnaipo-iposummary-title">IPO 개요</h2>
+            <div className="mnaipo-iposummary-title-line"></div>
+          </div>
+        </section>
+        <section className="mnaipo-iposummary-section">
+          <span className="mnaipo-iposummary-part">IPO 란</span>
+          <div>
+            IPO(Initial Public Offering)란 비상장기업이 정해진 절차에 따라 일반 불특정 다수의 투자자들에게 새로 주식을
+            발행하거나 기존 주식을 매출하여 유가증권시장 또는 코스닥시장에 상장하는 행위를 말합니다.
+          </div>
+        </section>{" "}
+        <section className="mnaipo-iposummary-section">
+          <span className="mnaipo-iposummary-part">IPO의 효과</span>
+          <div className="iposummary-effect">
+            <dl>
+              <dt>1. 필요자금 조달의 용이</dt>
+              <dd>
+                유상증자, 해외DR 발행, 전환사채, 교환사채 등 다양한 방법을 통해 대규모 필요자금을 쉽게 조달할 수
+                있습니다.
+              </dd>
+              <dt>2. 기업인지도 제고</dt>
+              <dd>
+                상장법인의 주가 등이 신문ㆍTV 등 언론매체에서 수시로 보도됨으로써 기업의 홍보효과가 극대화되고 국내외
+                투자자에 대한 당해 기업의 인지도를 제고할 수 있습니다. 또한 기업인지도 제고에 따라 우수 인재의 입사지원
+                증가 및 우수인력의 확보가 용이합니다.
+              </dd>
+              <dt>3. 기업구조조정의 원활한 추진</dt>
+              <dd>
+                상장법인의 분할 또는 합병 등에 의해 설립된 회사를 쉽게 상장할 수 있도록 상장요건 정비 등 환경을
+                조성함으로써 상장법인의 경영목적에 맞는 방법으로 구조조정을 원활하게 추진할 수 있습니다.
+              </dd>
+            </dl>
+          </div>
+        </section>
+        <section className="mnaipo-iposummary-section">
+          <span className="mnaipo-iposummary-part">IPO의 혜택</span>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>개업에 대한 혜택</th>
+                  <th>
+                    <dl>
+                      <dt>공모를 통한 유증상자 용이 (자본시장법 제165조의6)</dt>
+                      <dd>
+                        상장법인은 정관이 정하는 바에 따라 이사회의 결의로써 주주의 신주인수권을 배제하고 불특정
+                        다수인(당해 기업의 주주를 포함한다)을 상대로 하여 신주를 모집할 수 있습니다.
+                      </dd>
+                      <dt>의결권 없는 주식의 발행한도 특례 (자본시장법 제165조의15)</dt>
+                      <dd>
+                        비상장법인은 의결권 없는 주식을 발행주식총수의 25%까지 발행할 수 있습니다. 그러나 상장법인이
+                        외국에서 주식을 발행하거나 외국에서 발행한 해외전환사채, 해외신주인수권부사채, 기타 주식과
+                        관련된 증권 또는 증서의 권리행사로 발행하는 의결권 없는 주식은 발행한도의 계산에 산입되지
+                        않습니다.
+                      </dd>
+                      <dt>주식배당의 특례 (자본시장법 제165조의13)</dt>
+                      <dd>
+                        비상장법인은 주식배당을 배당가능이익액의 1/2를 초과하지 못하나, 상장법인은 배당가능 이익총액에
+                        상당하는 금액까지 주식배당을 할 수 있습니다.
+                      </dd>
+                      <dt>주주총회 소집절차의 간소화 (상법 제542조의4)</dt>
+                      <dd>
+                        비상장법인의 주주총회의 소집통지는 각 주주에게 서면으로 하게 되어 있으나, 상장법인의 주주총회
+                        소집통지는 의결권 있는 발행주식총수의 1%이하를 소유하는 주주에 대하여 정관이 정하는 바에 따라
+                        주주총회일 2주전에 2개 이상의 일간신문에 각각 2회 이상 공고함으로써 이를 갈음할 수 있습니다.
+                      </dd>
+                    </dl>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>주주에 대한 혜택</td>
+                  <td>
+                    <dl>
+                      <dt>주식양도소득에 대한 비과세 (소득세법 제94조, 동법시행령 제157조제4항)</dt>
+                      <dd>
+                        상장주식은 대주주 등을 제외하고 주식양도에 따른 양도소득세를 부과하지 않고 있기 때문에 상장을
+                        위해 모집하거나 유가증권시장을 통하여 양도하는 경우에는 양도소득세가 면제됩니다. 그러나
+                        비상장주식은 양도차익의 20%(중소기업의 경우 10%, 중소기업 이외의 법인의 주식을 소유한 대주주로서
+                        1년 미만 보유시 30%)를 세금으로 납부하여야 합니다.
+                      </dd>
+                      <dt> 상속 및 증여재산의 시가평가 (상속및증여세법 제63조 및 동법시행령 제53조)</dt>
+                      <dd>
+                        비상장법인이 발행한 주식을 상속 또는 증여할 경우 상속세 및 증여세법에서 정한 산식으로 평가하나,
+                        상장법인이 발행한 주식을 상속 또는 증여할 경우 평가기준일 전․후 각각 2개월간 최종시세의
+                        평균액으로 평가합니다.
+                      </dd>
+                      <dt>증권거래세 탄력세율 적용 (증권거래세법 제8조 및 동법시행령 제5조)</dt>
+                      <dd>
+                        비상장법인의 발행주식을 양도하는 경우 0.5%의 증권거래세율이 적용되지만, 유가증권시장을 통해
+                        양도되는 상장법인의 주식은 0.15%(0.15%의 농특세 추가부담)의 세율이 적용됩니다.
+                      </dd>
+                      <dt>소수주주권 행사완화 등 (상법 제542조의6, 제542조의7)</dt>
+                      <dd>상장법인의 경우 상법상 소수주주권 및 집중투표제 행사요건(1%) 등이 완화되어 적용됩니다.</dd>
+                    </dl>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </div>
       <DetailFooter />
     </div>
   );
