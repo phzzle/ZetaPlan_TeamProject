eaab0d53b6bd53a6a0307dbcf324f1ac295023e1
INJI <hoye7737@gmail.com>
2022-07-14 17:46:02 +0900

    industryTrade 컴포넌트 생성

---
+++ zetaplan/src/component/Industry/Trade/IndustryTrade4.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const IndustryTrade4 = () => {
+    return (
+        <div>
+            <div id='IndustryTrade'>
+                <section className='industry-trade-section'>
+                <span className="industry-trade-part">거래정보</span>
+                <p className='industry-trade-part-txt'></p>
+                </section>
+            </div>
+        </div>
+    );
+};
+
+export default IndustryTrade4;
