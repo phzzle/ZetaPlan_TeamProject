c00fea4bdde7cc4467cf51fc27b9e223ad9aface
phzzle <phzzle@gmail.com>
2022-07-14 16:50:45 +0900

    maresult, iposummary modify

--- zetaplan/src/page/MnAIpo/MAResult.jsx
+++ zetaplan/src/page/MnAIpo/MAResult.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import MAHeader from "../../component/Detail/MAHeader";
 import DetailFooter from "./../../component/Detail/DetailFooter";
 import "./../../css/MnAIpo/mnaresult.css";
+import MaResult1 from "../../component/MnaIpo/MaResult/MaResult2";
+import MaResult2 from "../../component/MnaIpo/MaResult/MaResult2";
+import {RESULT_DATA} from "../../component/MnaIpo/MaResult/ResultData";
 
 const MAResult = ({title, sub}) => {
+  const [content, setContent] = useState("first");
+
+  const buttonValueSetting = e => {
+    const {name} = e.target;
+    setContent(name);
+  };
+
+  const selectComponent = {
+    first: <MaResult1 />,
+    second: <MaResult2 />,
+  };
+
   return (
     <div id="maresult">
       <MAHeader title={title} sub={sub} />
@@ -12,6 +27,25 @@ const MAResult = ({title, sub}) => {
           <div className="mnaipo-maresult-title-container">
             <h2 className="mnaipo-maresult-title">M&A 절차 및 성과</h2>
             <div className="mnaipo-maresult-title-line"></div>
+          </div>
+        </section>
+        <section className="mnaipo-maresult-section">
+          <div>
+            <nav className="mnaipo-maresult-lists">
+              {RESULT_DATA.map(data => {
+                return (
+                  <button
+                    className={content === data.name ? "mna-result-list active" : "mna-result-list"}
+                    onClick={buttonValueSetting}
+                    name={data.name}
+                    key={data.id}
+                  >
+                    {data.text}
+                  </button>
+                );
+              })}
+            </nav>
+            {content && <div className="mnaipo-maresult-content">{selectComponent[content]}</div>}
           </div>
         </section>
       </div>
