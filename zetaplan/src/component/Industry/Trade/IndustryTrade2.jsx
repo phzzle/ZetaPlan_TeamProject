2a6dedac7d64fb6d9942b362e2ac93ffa26bc860
INJI <hoye7737@gmail.com>
2022-07-15 17:27:54 +0900

    img up

--- zetaplan/src/component/Industry/Trade/IndustryTrade2.jsx
+++ zetaplan/src/component/Industry/Trade/IndustryTrade2.jsx
@@ -4,13 +4,25 @@ const IndustryTrade2 = () => {
     return (
             <div id='IndustryTrade'>
                 <section className='industry-trade-section'>
-                <span className="industry-trade-part">기술판매</span>
-                <p className='industry-trade-part-txt'></p>
+                <span className="industry-trade-part"><strong>기술판매 컨설팅</strong></span>
+
+                <span className="industry-trade-part">기술수요자조사</span>
+                <p className='industry-trade-part-txt'>
+
+                    
+                </p>
+                <img src='/img/Industry/IndustryTrade/sellimg0.png' alt='sell' id='img0' />
+            
+                </section>
+                <section className='industry-trade-section'>
+                <span className="industry-trade-part"></span>
+                <p className='industry-trade-part-txt'>
                 <img src='/img/Industry/IndustryTrade/sellimg1.png' alt='sell' />
                 <img src='/img/Industry/IndustryTrade/sellimg2.png' alt='sell' />
                 <img src='/img/Industry/IndustryTrade/sellimg3.png' alt='sell' />
                 <img src='/img/Industry/IndustryTrade/sellimg4.png' alt='sell' id='img4' />
-                </section>
+                 </p>
+             </section>
             </div>
        
     );
