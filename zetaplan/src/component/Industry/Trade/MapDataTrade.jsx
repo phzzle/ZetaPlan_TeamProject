8e94bc06b470fd46ca068598af38601991169876
INJI <hoye7737@gmail.com>
2022-07-15 12:15:27 +0900

    IndustryTrade tab

---
+++ zetaplan/src/component/Industry/Trade/MapDataTrade.jsx
@@ -0,0 +1,22 @@
+export const MAP_DATA_TRADE = [
+  {
+  id: 1,
+  text: '기술거래',
+  name: 'first',
+  },
+  {
+  id: 2,
+  text: '기술구매',
+  name: 'second',
+  },
+  {
+    id: 3,
+    text: '기술판매',
+    name: 'third',
+  },
+  {
+    id: 4,
+    text: '거래정보',
+    name: 'fourth',
+  },
+  ];
--- zetaplan/src/page/Indusrty/IndustryTrade.jsx
+++ zetaplan/src/page/Indusrty/IndustryTrade.jsx
@@ -6,25 +6,48 @@ import IndustryTrade1 from '../../component/Industry/Trade/IndustryTrade1';
 import IndustryTrade2 from '../../component/Industry/Trade/IndustryTrade2';
 import IndustryTrade3 from '../../component/Industry/Trade/IndustryTrade3';
 import IndustryTrade4 from '../../component/Industry/Trade/IndustryTrade4';
-
+import { MAP_DATA_TRADE } from '../../component/Industry/Trade/MapDataTrade';
+import { useState } from 'react';
 
 
 
 const IndustryTrade = ({title, sub}) => {
+  const [content, setContent] = useState('first');
+
+  const buttonValueSetting = e => {
+    const { name } = e.target;
+    setContent(name);
+  };
+
+  const selectComponent = {
+    first: <IndustryTrade1 />,
+    second: <IndustryTrade2 />,
+    third: <IndustryTrade3 />,
+    fourth: <IndustryTrade4 />
+  };
+
   return (
     <div id='Trade'>
       <IndustryHeader title={title} sub={sub} />
       <div id='SubInner'>
-        <section className="industry-trade-section1">
+      <h2 className='detail-title'>기술 거래</h2>
+        <nav className='detail-tab-lists'>
+          {MAP_DATA_TRADE.map(data => {
+          return (
+            <button className={content === data.name? 'detail-tab-list active' : 'detail-tab-list'} onClick={buttonValueSetting} name={data.name} key={data.id}>
+              {data.text}
+            </button>
+          );
+        })}
+      </nav>
+      {content && <div className='detail-tab-content '>{selectComponent[content]}</div>}
+  {/*       <section className="industry-trade-section1">
             <div className="industry-trade-title-container">
               <h2 className="industry-trade-title">기술거래</h2>
               <div className="industry-trade-title-line"></div>
             </div>
-        </section>
-        <IndustryTrade1 />
-        <IndustryTrade2 />
-        <IndustryTrade3 />
-        <IndustryTrade4 />
+        </section> */}
+
       </div>
       <DetailFooter />
     </div>
