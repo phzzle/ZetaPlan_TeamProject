5e7e5d0374ffb36b8b397b83080cbed4c55e49e2
phzzle <phzzle@gmail.com>
2022-07-18 11:42:00 +0900

    modify sth...

--- zetaplan/src/page/MnAIpo/MAResult.jsx
+++ zetaplan/src/page/MnAIpo/MAResult.jsx
@@ -13,6 +13,10 @@ const MAResult = ({title, sub}) => {
             <h2 className="mnaipo-maresult-title">M&A 성과</h2>
             <div className="mnaipo-maresult-title-line"></div>
           </div>
+          <section className="mnaipo-maresult-section">
+            <span className="mnaipo-maresult-part">M&A 기법 및 성과</span>
+            <div className="mnaipo-maresult-part-img"></div>
+          </section>
         </section>
       </div>
       <DetailFooter />
