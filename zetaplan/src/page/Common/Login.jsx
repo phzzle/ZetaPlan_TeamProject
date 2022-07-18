b1128001a21e7057a196c4cd20cb3468f52426ce
JeongA Han <mongndam@naver.com>
2022-07-18 16:32:27 +0900

    Update Login.jsx

--- zetaplan/src/page/Common/Login.jsx
+++ zetaplan/src/page/Common/Login.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './../../component/Header/Header';
+import DetailFooter from './../../component/Detail/DetailFooter';
 
 const Login = () => {
   return (
     <div>
-      
+      <Header />
+      <div id='SubInner'></div>
+      <DetailFooter />
     </div>
   );
 };
