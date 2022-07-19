1f3069ccb8841ba7416f86a411e248af8c3fc119
JeongA Han <mongndam@naver.com>
2022-07-19 11:14:35 +0900

    산업 별 이슈 게시판 검색 기능 업로드

--- zetaplan/src/page/Indusrty/IndustryIssue.jsx
+++ zetaplan/src/page/Indusrty/IndustryIssue.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import DetailFooter from '../../component/Detail/DetailFooter';
 import IndustryHeader from '../../component/Detail/IndustryHeader';
 import IssueList from '../../component/Industry/Issue/IssueList';
+import BoardSearch6 from './../../component/Search/BoardSearch6';
 
 const IndustryIssue = ({title, sub}) => {
   return (
     <div id = 'IndustryIssue'>
       <IndustryHeader title={title} sub={sub} />
-      <div className="sub-inner">
-      <div className="company-title-container">
+      <div id="SubInner">
+        <div className="company-title-container">
           <h2 className='company-title'>산업별이슈</h2>
           <div className='company-title-line'></div>
         </div>
+        <BoardSearch6 />
         <IssueList />
       </div>
       <DetailFooter />
