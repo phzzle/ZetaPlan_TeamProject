39ed7b18030b081b5a764414965ad9b7e9a87844
phzzle <phzzle@gmail.com>
2022-07-19 20:14:21 +0900

    modify mnaipo

--- zetaplan/src/component/MnaIpo/IpoProcess/IpoProcess2.jsx
+++ zetaplan/src/component/MnaIpo/IpoProcess/IpoProcess2.jsx
@@ -20,7 +20,71 @@ const IpoProcess2 = () => {
       </section>
       <section className="mnaipo-ipoprocess-section">
         <span className="mnaipo-ipoprocess-part">상장요건 자문</span>
-        <div className="mnaipo-ipoprocess-advice-part"></div>
+        <div className="mnaipo-ipoprocess-advice-part">
+          <table>
+            <colgroup>
+              <col style={{width: "24%"}} />
+              <col style={{width: "24%"}} />
+              <col style={{width: "26%"}} />
+              <col style={{width: "26%"}} />
+            </colgroup>
+            <thead>
+              <tr>
+                <th>질적요건 심사표</th>
+                <th>영업위혐 평가서</th>
+                <th>경영지원시스템 평가표</th>
+                <th>대표주관업무 모범기준</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <ul>
+                    <li>시작성</li>
+                    <li>수익성</li>
+                    <li>재무상태</li>
+                    <li>기술성</li>
+                    <li>경영성</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>영업환경관련</li>
+                    <li>재무관련</li>
+                    <li>관계회사관련</li>
+                    <li>유통주식관련</li>
+                    <li>기타사항</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>최대주주 등에 관한 사항</li>
+                    <li>이해관계자에 관한 사항</li>
+                    <li>관계회사에 관한 사항</li>
+                    <li>이사회, 감사, 외부감사인 등에 관한 사항 </li>
+                    <li>회계처리에 관한 사항</li>
+                    <li>자금관리시스템에 관한 사항</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul>
+                    <li>회사의 개요</li>
+                    <li>자본</li>
+                    <li>주요제품의 시장성</li>
+                    <li>생산 (매입)</li>
+                    <li>영업활동 (매출)</li>
+                    <li>재무적 안정성</li>
+                    <li>관계회사</li>
+                    <li>자산관리</li>
+                    <li>지배구조, 이해관계자간 거래</li>
+                    <li>인사 및 노무</li>
+                    <li>기타사항</li>
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </section>
     </div>
   );
