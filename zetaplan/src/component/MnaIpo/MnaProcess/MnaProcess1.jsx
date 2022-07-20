60f3465515d41b798f38cebb71c882f8b0495b7b
phzzle <phzzle@gmail.com>
2022-07-20 12:18:47 +0900

    upgrade mnaprocess

--- zetaplan/src/component/MnaIpo/MnaProcess/MnaProcess1.jsx
+++ zetaplan/src/component/MnaIpo/MnaProcess/MnaProcess1.jsx
@@ -6,11 +6,150 @@ const MnaProcess1 = () => {
     <div id="mnaprocess" className="mnaprocess1">
       <section className="mnaipo-maprocess-section">
         <span className="mnaipo-maprocess-part">일반적인 절차</span>
-        <div className="mnaipo-maprocess-part-general"></div>
+        <div className="mnaipo-maprocess-part-general">
+          <div>
+            <div className="mnaipo-maprocess-part-general-num">01</div>
+            <div className="mnaipo-maprocess-part-general-content">
+              <h1>전략수립</h1>
+              <h2>산업 내 위치 파악, 장기 발전 계획 수립 M&A 전담부서 설치, 사외 자문 그룹 설치</h2>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-general-num">02</div>
+            <div className="mnaipo-maprocess-part-general-content">
+              <h1>대상 기업 선정</h1>
+              <h2>선정기준 마련, 대상업종 선정</h2>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-general-num">03</div>
+            <div className="mnaipo-maprocess-part-general-content">
+              <h1>대상 기업 가치 평가</h1>
+              <h2>다양한 방법 강구, 최고·최저가격 설정</h2>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-general-num">04</div>
+            <div className="mnaipo-maprocess-part-general-content">
+              <h1>자금 조달 방안 강구</h1>
+              <h2>조달 방법, 지급 방법</h2>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-general-num">05</div>
+            <div className="mnaipo-maprocess-part-general-content">
+              <h1>접촉과 교섭</h1>
+              <h2>가격 조정, 비밀 유지</h2>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-general-num">06</div>
+            <div className="mnaipo-maprocess-part-general-content">
+              <h1>기본의향서 교환</h1>
+              <h2>인수에 대한 원칙적 합의, 인수 일정 확인</h2>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-general-num">07</div>
+            <div className="mnaipo-maprocess-part-general-content">
+              <h1>정밀 실사</h1>
+              <h2>전문가 진단, 체크리스트 활용</h2>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-general-num">08</div>
+            <div className="mnaipo-maprocess-part-general-content">
+              <h1>계약의 체결</h1>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-general-num">09</div>
+            <div className="mnaipo-maprocess-part-general-content">
+              <h1>대금 지급 및 사후 관리</h1>
+            </div>
+          </div>
+        </div>
       </section>
       <section className="mnaipo-maprocess-section">
         <span className="mnaipo-maprocess-part">중개기관을 통한 절차</span>
-        <div className="mnaipo-maprocess-part-broker"></div>
+        <div className="mnaipo-maprocess-part-broker">
+          <div>
+            <div className="mnaipo-maprocess-part-broker-num">01</div>
+            <div className="mnaipo-maprocess-part-broker-content">
+              <h1>
+                정보 매칭 <span>( 비밀유지약정 )</span>
+              </h1>
+              <ul>
+                <li>매수자 : 매수희망사업, 매수희망가격 제시</li>
+                <li>매도자 : 매도기업 자료 제공, 매도희망가격 제시</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-broker-num">02</div>
+            <div className="mnaipo-maprocess-part-broker-content">
+              <h1>
+                대상 기업 가치 평가 <span>( 중개인 계약 체결 )</span>
+              </h1>
+              <ul>
+                <li>매수자 : 매수대상기업의 사업타당성 및 합목적성 평가</li>
+                <li>매도자 : 해당산업 분석 자료 제공, 매도기업 세부자료 제공</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-broker-num">03</div>
+            <div className="mnaipo-maprocess-part-broker-content">
+              <h1>자금 조달 방안 강구</h1>
+              <ul>
+                <li>매수자 : 매수의향의 표시, 매수조항의 수정, 비밀유지 계약</li>
+                <li>매도자 : 매도의향의 표시, 매도조항의 수정, 비밀유지 계약</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-broker-num">04</div>
+            <div className="mnaipo-maprocess-part-broker-content">
+              <h1>접촉과 교섭</h1>
+              <ul>
+                <li>매수자 : 제공자료의 검증여부 판단, 매수가치 평가, 필요시 외부기관 동행실사</li>
+                <li>매도자 : 매도기업 정밀실사자료 제공, 매도기업 실사여건 협조</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-broker-num">05</div>
+            <div className="mnaipo-maprocess-part-broker-content">
+              <h1>기본의향서 교환</h1>
+              <ul>
+                <li>매수자 : 법률 및 세무문제 등 구체적인 매수조건 협의</li>
+                <li>매도자 : 법률 및 세무문제 등 구체적인 매도조건 협의</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-broker-num">06</div>
+            <div className="mnaipo-maprocess-part-broker-content">
+              <h1>
+                정밀 심사 <span> ( 자금조달 자문 ) </span>
+              </h1>
+              <ul>
+                <li>매수자 : 최종매수가 협상, 계약서 작성, 계약서 내용 최종 확인</li>
+                <li>매도자 : 최종매도가 협상, 계약서 작성, 계약서 내용 최종 확인</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <div className="mnaipo-maprocess-part-broker-num">07</div>
+            <div className="mnaipo-maprocess-part-broker-content">
+              <h1>계약의 체결</h1>
+              <ul>
+                <li>매수자 : 매수의무 실행, 매수기업 경영</li>
+                <li>매도자 : 매도의무 실행</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
