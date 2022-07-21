080134a4d38611b329344111118f8ed0c06337c2
INJI <hoye7737@gmail.com>
2022-07-22 00:55:09 +0900

    info 미디어쿼리, aos

--- zetaplan/src/component/MainFullpage/MainFullpageInfo/MainFullpageInfo.jsx
+++ zetaplan/src/component/MainFullpage/MainFullpageInfo/MainFullpageInfo.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import './mainFullpageInfo.css';
 import { ImArrowRight2 } from 'react-icons/im';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 const MainFullpageInfo = () => {
+  useEffect(() => {
+    AOS.init({
+        duration : 2000
+    });
+});
+
   return (
     <div id='fullpageInfo'>
-      <div id='newsMainInner'>
+      <div id='MainInner'>
+
         <div className="main-info-container">
-        <div className='main-info-box'>
-          <div className='main-info-title-box'>
+        <div className='main-info-title-box'>
             <h2 className='main-info-title'>
               <span> BUSINESS</span>
               사업소개
             </h2>
           </div>
-          <div className='fullpageInfo-boxs'>
+        <div className='main-info-box'>
+       
+          <div className='fullpageInfo-boxs'  data-aos="fade-up">
             <div className='ir-box' id='navy'>
               <h2>투자IR</h2>
               <p className='info-detail'>
@@ -23,7 +34,7 @@ const MainFullpageInfo = () => {
               <div className='detail-box'>
                 <a href='/invest/result'>
                   <p>
-                    <ImArrowRight2 /> 자세히보기
+                    <ImArrowRight2 /> <span id='blind'>자세히보기</span>
                   </p>
                 </a>
               </div>
@@ -32,14 +43,14 @@ const MainFullpageInfo = () => {
               <h2>M&A</h2>
               <p className='info-detail'>
                 {' '}
-                제휴 네트워크의 IB, 로펌, 회계법인으로 구성된 <br /> 최고의 M&A
+                제휴 네트워크의 IB, 로펌, 회계법인으로 구성된 최고의 M&A
                 팀
               </p>
               <div className='detail-box'>
                 <a href='/mna'>
                   <p>
                     {' '}
-                    <ImArrowRight2 fill='rgb(43, 53, 64)' /> 자세히보기{' '}
+                    <ImArrowRight2 fill='rgb(43, 53, 64)' /><span id='blind'>자세히보기</span>
                   </p>
                 </a>
               </div>
@@ -52,7 +63,7 @@ const MainFullpageInfo = () => {
               <div className='detail-box'>
                 <a href='/ipo'>
                   <p>
-                    <ImArrowRight2 /> 자세히보기
+                    <ImArrowRight2 /> <span id='blind'>자세히보기</span>
                   </p>
                 </a>
               </div>
@@ -63,7 +74,7 @@ const MainFullpageInfo = () => {
               <div className='detail-box'>
                 <a href='/abroad/process'>
                   <p>
-                    <ImArrowRight2 fill='rgb(43, 53, 64)' /> 자세히보기
+                    <ImArrowRight2 fill='rgb(43, 53, 64)' /> <span id='blind'>자세히보기</span>
                   </p>
                 </a>
               </div>
@@ -71,13 +82,13 @@ const MainFullpageInfo = () => {
             <div className='ir-box' id='navy'>
               <h2>경영컨설팅</h2>
               <p className='info-detail'>
-                `비용의 최소화 및 이익의 최대화`를 실현하는 <br /> 비즈니스
-                서비스 컨설팅{' '}
+                `비용의 최소화 및 이익의 최대화` 실현 비즈니스
+                서비스 컨설팅
               </p>
               <div className='detail-box'>
                 <a href='/invest'>
                   <p>
-                    <ImArrowRight2 /> 자세히보기
+                    <ImArrowRight2 /><span id='blind'>자세히보기</span>
                   </p>
                 </a>
               </div>
@@ -87,7 +98,7 @@ const MainFullpageInfo = () => {
               <div className='detail-box'>
                 <a href='#'>
                   <p>
-                    <ImArrowRight2 fill='rgb(43, 53, 64)' /> 자세히보기
+                    <ImArrowRight2 fill='rgb(43, 53, 64)' /> <span id='blind'>자세히보기</span>
                   </p>
                 </a>
               </div>
