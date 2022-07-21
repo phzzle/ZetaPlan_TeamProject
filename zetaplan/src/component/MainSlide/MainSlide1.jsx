59a8d9ee06eddfbda56c948b6035e31eab3d78e4
JeongA Han <mongndam@naver.com>
2022-07-21 12:31:16 +0900

    미디어쿼리 수정

--- zetaplan/src/component/MainSlide/MainSlide1.jsx
+++ zetaplan/src/component/MainSlide/MainSlide1.jsx
@@ -6,9 +6,64 @@ import './mainSlide.css';
 const MainSlide1 = () => {
   return (
     <div id='MainSlide1'>
-      <div className='main-slide-wrap'></div>
+      <div className='main-slide-wrap'>
+        <div className="main-slide-night">
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+          <div className='main-slide-star'></div>
+        </div>
+      </div>
       <span className='main-slide-by'>ZetaPlanInvestment</span>
-      <div className='main-slide-title'>ZETA PLAN</div>
+      <div className='main-slide-title'>
+        <div className='main-slide-title-wrap'>
+          <span>Z</span>
+          <span>E</span>
+          <span>T</span>
+          <span>A</span>
+          <span>P</span>
+          <span>L</span>
+          <span>A</span>
+          <span>N</span>
+        </div>
+      </div>
       <Link to='/company' className='main-slide-btn'><GrNext size='30px' />More</Link>
     </div>
   );
