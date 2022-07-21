190e4b63081790be2c3d162e10359a967af1bf7f
phzzle <phzzle@gmail.com>
2022-07-21 20:01:53 +0900

    footer css 반응형

--- zetaplan/src/component/Footer/Footer.jsx
+++ zetaplan/src/component/Footer/Footer.jsx
@@ -15,12 +15,12 @@ const Footer = () => {
             중소기업상담회사 제2010-397호 | 기술거래기관 제2016-48호 | 사업화전문회사 제2017-96호 | 엑셀러레이터
             제2018-60호
           </div>
-          <div className="footer_paddingbtm">
+          <div className="footer_paddingbtm_center">
             <strong>서울본사</strong> (08589) 서울 금천구 디지털로 130, 1210 | Tel 02-538-4801 | Fax 02-6008-4779 |
             E-mail zetabiz@zetaplan.com <br />
             <strong>호남본부</strong> (61947) 광주광역시 서구 상무중앙로 114, 1307호 | Tel 062-233-4142
           </div>
-          <div>
+          <div className="footer_paddingbtm_btm">
             <strong>China Office</strong> 6F, Beiguang Building, No.23, Huangsi Street, Xicheng District, Beijing, China
             <br />
             <strong>Vietnam Office </strong> No 241, Tang Thiet Giap Apt, 110 Hoang Quoc Viet Street, Cau Giay District,
