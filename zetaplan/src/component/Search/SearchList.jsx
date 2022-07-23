9f786967571ab15f1a7e09a53573fc9fd9e77aab
JeongA Han <mongndam@naver.com>
2022-07-23 23:33:26 +0900

    Update SearchList.jsx

--- zetaplan/src/component/Search/SearchList.jsx
+++ zetaplan/src/component/Search/SearchList.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 const SearchList = ({data}) => {
   return (
     <li className='search-lists'>
-      <Link to={data.link} className='search-list'>
+      <a href={data.link} className='search-list'>
         <span className='inner-search-sub'>{data.sub}</span>
         <span className='inner-search-num'>{data.num}</span>
         <span className='inner-search-title'>{data.title}</span>
         <span className='inner-search-date'>{data.date}</span>
-      </Link>
+      </a>
     </li>
   );
 };
